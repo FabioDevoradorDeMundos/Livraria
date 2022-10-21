148373e718deb9cc7a5ffd412e4aae77af987e42
FabioDevoradorDeMundos <111889081+FabioDevoradorDeMundos@users.noreply.github.com>
2022-10-21 20:06:29 -0300

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://Fabio:va1sefude@cluster0.s20xqwp.mongodb.net/Cluster0");
+mongoose.connect("mongodb+srv://Fabio:******@cluster0.s20xqwp.mongodb.net/Cluster0");
 
 let db = mongoose.connection;
 
